4ad136c9f8265801a3cb840aef383173ce2ac634
Malxy <52326823+Malxy@users.noreply.github.com>
2022-08-17 18:35:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,17 +18,5 @@ function w3_close() {
 }
 
       
-let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 46.568755703959496, lng: 26.91262871696031 },
-    zoom: 20,
-    panControl: false,
-    streetViewControl: false,
-    overlayMapTypes:false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-}
 
-window.initMap = initMap;
